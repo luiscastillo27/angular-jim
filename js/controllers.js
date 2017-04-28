9848b7b98120da3b6ab6e7a7d2ac043aad6a8f7d
luiscastillo27 <luisyosemite@gmail.com>
2017-04-28 14:58:06 -0500

    calculadora
    una calculadora

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,11 @@
 var app = angular.module('listControllers', []);
 
 app.controller('InicioCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log("TE AMO BB");
+    
+    $scope.sumar = function(){
+    	var resp = $scope.num1;
+    	$scope.resultado = resp;
+    }
+
+
 }]);
