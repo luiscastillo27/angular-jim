970dd4d1ffdb140c36d3550ab5b970b75f21504d
Mirellitha27 <anamireyaj@gmail.com>
2017-05-08 19:24:11 -0500

    calculadora
    no entiendo por que si hace la resta bien y la suma no.
    Los otros operadores pues no funcionan :(

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,31 @@ var app = angular.module('listControllers', []);
 app.controller('InicioCtrl', ['$scope', '$http', function ($scope, $http) {
     
     $scope.sumar = function(){
-    	var resp = $scope.num1;
+    	var r1 = $scope.num1;
+    	var r2 = $scope.num2;
+    	var resp = r1 + r2;
     	$scope.resultado = resp;
     }
+
+    $scope.resta = function(){
+    	var r1 = $scope.num1;
+    	var r2 = $scope.num2;
+    	var resp = r1 - r2;
+    	$scope.resultado = resp;
+    }
+    $scope.multiplica = function(){
+        var r1 = $scope.num1;
+        var r2 = $scope.num2;
+        var resp = r1 * r2;
+        $scope.resultado = resp;
+    }
+     $scope.divide = function(){
+        var r1 = $scope.num1;
+        var r2 = $scope.num2;
+        var resp = r1 / r2;
+        $scope.resultado = resp;
+    }
+
 
 
 }]);
